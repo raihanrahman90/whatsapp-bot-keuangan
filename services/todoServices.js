ede620f11bfe8e714626c131d29bbfffa74330d4
Muhammad Raihan Rahman <muhammad.raihan.ecomindo@external.bankmas.co.id>
2026-07-19 11:39:24 +0800

    update todo

--- services/todoServices.js
+++ services/todoServices.js
@@ -1,7 +1,6 @@
 const fs = require("fs");
 const path = require("path");
 
-const FILE_NAME = path.join(__dirname, "../todos.json");
 const FILE_NAME = path.join(process.cwd(), "todos.json");
 
 function loadTodos() {
